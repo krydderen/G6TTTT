48aef9088f647da3e0e5d950f149aad1e79d5d76
Kevin <kevinms@stud.ntnu.no>
2019-11-05 19:35:09 +0100

    day1/made the code look better ++
    Reduced the amount of lines ROI takes, will continue this tomorrow.

--- TripleT.py
+++ TripleT.py
@@ -19,67 +19,49 @@ img = cv.imread(IMAGE, cv.IMREAD_GRAYSCALE)
 
 # Cropping the image with HEIGHTNR1
 roi = img2[y1:y2, x1:x2]
-whidth = len(roi)
-height = len(roi[0])
+
+# Collect the height and width of the image
+height, width = roi.shape[:2]
+
+# Make a list where we can add the following tiles
 rect_list = list()
 
+# Tile number #1
 rect_list.append(
-    cv.rectangle(roi,
-                 (0, 0),
-                 (int(whidth / 3), int(height / 3)),
-                 (0, 255, 0),
-                 thickness=10))
+    cv.rectangle(roi,(0, 0),(int(width / 3), int(height / 3)),(0, 255, 0),thickness=10))
+# Tile number #2
 rect_list.append(
-    cv.rectangle(roi,
-                 (int(whidth / 3), 0),
-                 (int(whidth * 2 / 3), int(height / 3)),
-                 (0, 255, 0),
-                 thickness=10))
+    cv.rectangle(roi,(int(width / 3), 0),(int(width * 2 / 3), int(height / 3)),(0, 255, 0),thickness=10))
+# Tile number #3
 rect_list.append(
-    cv.rectangle(roi,
-                 (int(whidth * 2 / 3), 0),
-                 (int(whidth), int(height / 3)),
-                 (0, 255, 0),
-                 thickness=10))
+    cv.rectangle(roi,(int(width * 2 / 3), 0),(int(width), int(height / 3)),(0, 255, 0),thickness=10))
+# Tile number #4
 rect_list.append(
-    cv.rectangle(roi,
-                 (0, int(height / 3)),
-                 (int(whidth / 3), int(height * 2 / 3)),
-                 (0, 255, 0),
-                 thickness=10))
+    cv.rectangle(roi,(0, int(height / 3)),(int(width / 3), int(height * 2 / 3)),(0, 255, 0),thickness=10))
+# Tile number #5
 rect_list.append(
-    cv.rectangle(roi,
-                 (int(whidth / 3), int(height / 3)),
-                 (int(whidth * 2 / 3), int(height * 2 / 3)),
-                 (0, 255, 0),
-                 thickness=10))
+    cv.rectangle(roi,(int(width / 3), int(height / 3)),(int(width * 2 / 3), int(height * 2 / 3)),(0, 255, 0),thickness=10))
+# Tile number #6
 rect_list.append(
-    cv.rectangle(roi,
-                 (int(whidth * 2 / 3), int(height / 3)),
-                 (int(whidth), int(height * 2 / 3)),
-                 (0, 255, 0),
-                 thickness=10))
+    cv.rectangle(roi,(int(width * 2 / 3), int(height / 3)),(int(width), int(height * 2 / 3)),(0, 255, 0),thickness=10))
+# Tile number #7
+rect_list.append(
+    cv.rectangle(roi,(0, int(height * 2 / 3)),(int(width / 3), int(height)),(0, 255, 0),thickness=10))
+# Tile number #8
+rect_list.append(
+    cv.rectangle(roi,(int(width / 3), int(height * 2 / 3)),(int(width * 2 / 3), int(height)),(0, 255, 0),thickness=10))
+# Tile number #9
+rect_list.append(
+    cv.rectangle(roi,(int(width * 2 / 3), int(height * 2 / 3)),(int(width), int(height)),(0, 255, 0),thickness=10))
 
-rect_list.append(
-    cv.rectangle(roi,
-                 (0, int(height * 2 / 3)),
-                 (int(whidth / 3), int(height)),
-                 (0, 255, 0),
-                 thickness=10))
-
-rect_list.append(
-    cv.rectangle(roi,
-                 (int(whidth / 3), int(height * 2 / 3)),
-                 (int(whidth * 2 / 3), int(height)),
-                 (0, 255, 0),
-                 thickness=10))
-
-rect_list.append(
-    cv.rectangle(roi,
-                 (int(whidth * 2 / 3), int(height * 2 / 3)),
-                 (int(whidth), int(height)),
-                 (0, 255, 0),
-                 thickness=10))
+tileCount = 0
+x = 50
+y = 50
+for tile in rect_list:
+    tileCount += 1
+    cv.putText(roi, str(tileCount), (x, y) , cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), thickness=5)
+    x += 50
+    y += 50
 
 # Converting image to a binary image
 # (black and white only image)
@@ -88,42 +70,6 @@ _, threshold = cv.threshold(roi, 110, 255, cv.THRESH_BINARY)
 # Turn into threshold binary
 ret, threshold1 = cv.threshold(roi, 127, 255, cv.THRESH_BINARY)
 
-# Detecting shapes in image by selecting region
-# with same colors or intensity.
-# contours, _ = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-"""
-# Find and draw contours. RETR_EXTERNAL retrieves only the extreme outer contrours.
-contours, hierarchy = cv.findContours(threshold1, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-
-cv.drawContours(roi, contours, -1, (0, 255, 0), 15)
-"""
-"""tileCount = 0
-for cnt in contours:
-    # ignore small contours that are not tiles
-    if cv.contourArea(cnt) > 200000:
-        tileCount = + 1
-        # Use boundingRect to get coordinates of the tile
-        x, y, w, h = cv.boundingRect(cnt)
-        # Create a new image from binary, for further analysis. Trim of the edges that has a line.
-        tile = threshold1[x + 40:x + w - 80, y + 40:y + h - 80]
-        # Create a new image from the main image, so we can draw the contours easily
-        imgTile = roi[x + 40:x + w - 80, y + 40:y + h - 80]
-
-        # Determine the array indexes of the tile
-        # TODO add a function to add the tiles
-
-        # Put a number in each tile. TODO HAVE TO GET IT TO PUT A NUMBER CORRECTLY.
-        cv.putText(roi, str(tileCount), (x + 200, y + 300), cv.FONT_HERSHEY_SIMPLEX, 10, (0, 0, 255), 20)
-
-
-# Print the gamestate
-print("Gamestate: ")
-for line in GAMESTATE:
-    linetxt = ""
-    for cel in line:
-        linetxt = linetxt + "|" + cel
-        print(linetxt)
-"""
 # Resize the final image
 res = cv.resize(roi, None, fx=0.8, fy=0.8, interpolation=cv.INTER_CUBIC)
 
