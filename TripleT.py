5e201d34abcb757bebe3adebcf298c8e45bf0a5a
Kevin <kevinms@stud.ntnu.no>
2019-11-05 17:03:05 +0100

    day1/finished making ROIs
    TODO 
    - Check each tile for change

--- TripleT.py
+++ TripleT.py
@@ -1,10 +1,10 @@
 import cv2 as cv
 import numpy
 
-x1 = 625
-x2 = 1325
-y1 = 115
-y2 = 795
+x1 = 630
+x2 = 1320
+y1 = 120
+y2 = 790
 
 GAMESTATE = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
 
@@ -18,7 +18,68 @@ img2 = cv.imread(IMAGE, cv.IMREAD_COLOR)
 img = cv.imread(IMAGE, cv.IMREAD_GRAYSCALE)
 
 # Cropping the image with HEIGHTNR1
-roi = img2[y1:y2 , x1:x2]
+roi = img2[y1:y2, x1:x2]
+whidth = len(roi)
+height = len(roi[0])
+rect_list = list()
+
+rect_list.append(
+    cv.rectangle(roi,
+                 (0, 0),
+                 (int(whidth / 3), int(height / 3)),
+                 (0, 255, 0),
+                 thickness=10))
+rect_list.append(
+    cv.rectangle(roi,
+                 (int(whidth / 3), 0),
+                 (int(whidth * 2 / 3), int(height / 3)),
+                 (0, 255, 0),
+                 thickness=10))
+rect_list.append(
+    cv.rectangle(roi,
+                 (int(whidth * 2 / 3), 0),
+                 (int(whidth), int(height / 3)),
+                 (0, 255, 0),
+                 thickness=10))
+rect_list.append(
+    cv.rectangle(roi,
+                 (0, int(height / 3)),
+                 (int(whidth / 3), int(height * 2 / 3)),
+                 (0, 255, 0),
+                 thickness=10))
+rect_list.append(
+    cv.rectangle(roi,
+                 (int(whidth / 3), int(height / 3)),
+                 (int(whidth * 2 / 3), int(height * 2 / 3)),
+                 (0, 255, 0),
+                 thickness=10))
+rect_list.append(
+    cv.rectangle(roi,
+                 (int(whidth * 2 / 3), int(height / 3)),
+                 (int(whidth), int(height * 2 / 3)),
+                 (0, 255, 0),
+                 thickness=10))
+
+rect_list.append(
+    cv.rectangle(roi,
+                 (0, int(height * 2 / 3)),
+                 (int(whidth / 3), int(height)),
+                 (0, 255, 0),
+                 thickness=10))
+
+rect_list.append(
+    cv.rectangle(roi,
+                 (int(whidth / 3), int(height * 2 / 3)),
+                 (int(whidth * 2 / 3), int(height)),
+                 (0, 255, 0),
+                 thickness=10))
+
+rect_list.append(
+    cv.rectangle(roi,
+                 (int(whidth * 2 / 3), int(height * 2 / 3)),
+                 (int(whidth), int(height)),
+                 (0, 255, 0),
+                 thickness=10))
 
 # Converting image to a binary image
 # (black and white only image)
