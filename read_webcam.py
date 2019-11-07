0b39833741c7b78b4f063dbdfd771d802da53835
KMS <kevinms@stud.ntnu.no>
2019-11-07 14:53:37 +0100

    day3/

--- read_webcam.py
+++ read_webcam.py
@@ -1,3 +1,4 @@
+    cv2.imshow("frame", frame)
 import cv2
 import numpy as np
 
@@ -6,7 +7,20 @@ cap = cv2.VideoCapture(1)
 while True:
     ret, frame = cap.read()
 
-    cv2.imshow("frame", frame)
+    roi = frame[80:386, 168:470]
+
+    tiles = [roi[0:100, 0:100],
+             roi[0:100, 100:200],
+             roi[0:100, 200:300],
+             roi[100:200, 0:100],
+             roi[100:200, 100:200],
+             roi[100:200, 200:300],
+             roi[200:300, 0:100],
+             roi[200:300, 100:200],
+             roi[200:300, 200:300]]
+
+    
+    cv2.imshow("frame", roi)
 
     key = cv2.waitKey(1)
     if key == 27:
