da1907a70eebd1e9f22d1ce8b35bb9aef2499c7f
KMS <kevinms@stud.ntnu.no>
2019-11-13 20:07:58 +0100

    day5/ modbus shenanigans
    Added a program assigned to push on and off light switch, and open door

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Importing packages
 from modbus_communication import ModbusClient
+import time
 
 # Dictionary holding Modbus addresses
 addresses = {
     '0': 140,
     '1': 141,
-    '2': 142
+    '2': 142,
+    'kkk': 148
 }
 
 
@@ -16,8 +18,20 @@ if __name__ == '__main__':
     print("Client connecting...")
     while client.isConnected():
         print("Client connected.")
-        client.sendInt(value=1, address=addresses['0'])
-        response = client.readInt(address=addresses['0'], size=1)
+        cmd = input("ENTER CMD\n")
+
+        if str(cmd) == "EXIT":
+            print("Shutting down...")
+            break
+
+        client.sendInt(value=(int(cmd)), address=addresses['kkk'])
+        response = client.readInt(address=addresses['kkk'], size=1)
         print(response)
-        break
+
+        time.sleep(1)
+        # Resetting to 0
+        client.sendInt(value=0, address=addresses['kkk'])
+        response = client.readInt(address=addresses['kkk'], size=1)
+        print(response)
+
     client.close()
